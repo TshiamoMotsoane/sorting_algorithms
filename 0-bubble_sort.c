92285889d344b35090b8e1b7095e5eb51ea2f7cc
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-02-16 02:35:36 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,44 @@
 #include "sort.h"
 
 /**
+ * swap_ints - Swap two integers in an array.
+ * @a: int to be swapped
+ * @b: int to be swapped
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * bubble_sort - Sorts an array of integers in ascending oder.
  * @array: The array to be sorted,
  * @size: Number of elements in the array
  */
-
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j = size;
-	bool swapped;
-	int temp;
+	bool swapped = false;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; ++i)
+	while (swapped == false)
 	{
-		swapped = false;
-		for (j = 0; j < size - i - 1; ++j)
+		swapped = true;
+		for (i = 0; i < j - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				swapped = true;
+				swapped = false;
 			}
 		}
-		if (!swapped)
-			break;
+		j--;
 	}
 }
